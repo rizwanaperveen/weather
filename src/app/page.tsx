06f33958a11665a4458963c0e56d597faf3fd2f6
Rizwana <159348120+rizwanaperveen@users.noreply.github.com>
2024-10-02 17:39:44 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import WeatherWidget from "../components/ui/weather";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <WeatherWidget />
+    </main>
+  );
+}
